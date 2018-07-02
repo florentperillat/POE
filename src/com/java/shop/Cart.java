682d743d57dd73ec52663b91cbd2688c52c159e2
florentperillat <florentper@gmail.com>
2018-07-02 11:03:13 +0200

    MediaRow

--- src/com/java/shop/Cart.java
+++ src/com/java/shop/Cart.java
@@ -3,7 +3,7 @@ package com.java.shop;
 import java.util.ArrayList;
 
 public class Cart {
-    ArrayList<MediaCopies> list = new ArrayList<>();
+    ArrayList<MediaRow> list = new ArrayList<>();
     double totalPriceTTC;
 
     public Cart(){ //le pannier se construit toujours vide :pas de paramètre dans le constructeur
@@ -11,12 +11,12 @@ public class Cart {
     }
 
     public void add(Media media, int nb){
-        list.add(new MediaCopies(media, nb));
+        list.add(new MediaRow(media, nb));
     }
 
     public double getTotalPriceTTC() {
         totalPriceTTC = 0;
-        for (MediaCopies media:list) {
+        for (MediaRow media:list) {
             totalPriceTTC += media.media.getNetPrice()*media.nbCopies;
         }
         return totalPriceTTC;
